5a71280f2917674c19a2df23719c27b266099e01
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2023-06-13 08:44:13 -0300

    primeiro teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnuncioComponent } from './anuncio/anuncio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnuncioComponent
   ],
   imports: [
     BrowserModule
